8ccdcdbc3ecb7baf40b9aaa883606b5a99d54475
tchang135 <tchang135@gmail.com>
2022-11-16 14:33:23 -0500

    finished board.rb

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,38 @@
+require "byebug"
+
 class Board
-  
+    def initialize(n)
+        @grid = Array.new(n) {Array.new(n,:N)}
+        @size = n * n 
+    end 
+
+    
+    def size 
+        @size 
+    end 
+
+
+    def [](arr)
+        first = arr[0]
+        second = arr[-1]   
+        @grid[first][second]
+    end 
+
+
+    def []=(pos, val)
+        first = pos[0]
+        second = pos[-1]
+        @grid[first][second] = val
+    end 
+
+
+    def num_ships
+        counter = 0
+        @grid.each do |subarr|
+            subarr.each do |ele|
+                counter += 1 if ele == :S
+            end 
+        end 
+    counter
+    end 
 end
