8cafe178fb259b2a4ff9721468e6dbe8724b50bf
tchang135 <tchang135@gmail.com>
2022-11-16 14:43:05 -0500

    finished player.rb

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,14 @@
+require "byebug"
+
 class Player
-    
+    def get_move 
+        puts 'enter a position with coordinates separated with a space like `4 7`'
+        input = gets.chomp 
+        coords = input.split(" ")
+        arr = []
+        coords.each do |num|
+            arr << num.to_i
+        end 
+    arr
+    end 
 end
